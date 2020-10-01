01267b32ff168985b1cd1f9da9aca27f72930567
Nur Rifqi Alhusaini <rifqifoc@gmail.com>
2020-10-02 03:49:53 +0700

    Add project files.

---
+++ CalculatorDesktopApp/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CalculatorDesktopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbOperasi = new System.Windows.Forms.ComboBox();
+            this.txtNilaiA = new System.Windows.Forms.TextBox();
+            this.txtNilaiB = new System.Windows.Forms.TextBox();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nilai A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nilai B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hasil";
+            // 
+            // cmbOperasi
+            // 
+            this.cmbOperasi.AllowDrop = true;
+            this.cmbOperasi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperasi.FormattingEnabled = true;
+            this.cmbOperasi.Items.AddRange(new object[] {
+            "Pertambahan",
+            "Pengurangan",
+            "Perkalian",
+            "Pembagian"});
+            this.cmbOperasi.Location = new System.Drawing.Point(100, 22);
+            this.cmbOperasi.Name = "cmbOperasi";
+            this.cmbOperasi.Size = new System.Drawing.Size(194, 28);
+            this.cmbOperasi.TabIndex = 1;
+            // 
+            // txtNilaiA
+            // 
+            this.txtNilaiA.Location = new System.Drawing.Point(101, 66);
+            this.txtNilaiA.Name = "txtNilaiA";
+            this.txtNilaiA.Size = new System.Drawing.Size(193, 27);
+            this.txtNilaiA.TabIndex = 2;
+            // 
+            // txtNilaiB
+            // 
+            this.txtNilaiB.Location = new System.Drawing.Point(101, 104);
+            this.txtNilaiB.Name = "txtNilaiB";
+            this.txtNilaiB.Size = new System.Drawing.Size(193, 27);
+            this.txtNilaiB.TabIndex = 3;
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Enabled = false;
+            this.txtHasil.Location = new System.Drawing.Point(100, 148);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.ReadOnly = true;
+            this.txtHasil.ShortcutsEnabled = false;
+            this.txtHasil.Size = new System.Drawing.Size(194, 27);
+            this.txtHasil.TabIndex = 5;
+            this.txtHasil.TabStop = false;
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(15, 193);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(279, 40);
+            this.btnHitung.TabIndex = 4;
+            this.btnHitung.Text = "Hitung";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 253);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.txtNilaiB);
+            this.Controls.Add(this.txtNilaiA);
+            this.Controls.Add(this.cmbOperasi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbOperasi;
+        private System.Windows.Forms.TextBox txtNilaiA;
+        private System.Windows.Forms.TextBox txtNilaiB;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Button btnHitung;
+    }
+}
+
---
+++ CalculatorDesktopApp/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorDesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHitung_click(object sender, EventArgs e)
+        {
+            var a = float.Parse(txtNilaiA.Text);
+            var b = float.Parse(txtNilaiB.Text);
+            Double hasil;
+
+            switch (cmbOperasi.SelectedIndex)
+            {
+                case 0:
+                    hasil = a + b;
+                    txtHasil.Text = Convert.ToString(hasil);
+                    break;
+                case 1:
+                    hasil = a - b;
+                    txtHasil.Text = Convert.ToString(hasil);
+                    break;
+                case 2:
+                    hasil = a * b;
+                    txtHasil.Text = Convert.ToString(hasil);
+                    break;
+                case 3:
+                    hasil = a / b;
+                    txtHasil.Text = Convert.ToString(hasil);
+                    break;
+
+            }
+        }
+    }
+}
